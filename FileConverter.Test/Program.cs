7af90ef2a2cb802443e310c6d5c25a6fc7ba7e60
Starinin Andrey <starininandrey@gmail.com>
2021-12-02 14:21:48 +0300

    Add Handle Test proj

---
+++ FileConverter.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileConverter.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
