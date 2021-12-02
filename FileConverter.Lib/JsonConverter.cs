7020eee46aa45cb5c10259f7862c65b8103e279f
Starinin Andrey <starininandrey@gmail.com>
2021-12-02 14:18:05 +0300

    Init Lib proj

---
+++ FileConverter.Lib/JsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+
+namespace FileConverter.Lib
+{
+    public class JsonConverter
+    {
+        public void OnParse(string str)
+        {
+            using var doc = JsonDocument.Parse(str);
+            var root = doc.RootElement;
+
+            var elements = root.EnumerateArray();
+
+            while (elements.MoveNext())
+            {
+                var element = elements.Current;
+                Console.WriteLine(elements);
+
+                var props = element.EnumerateObject();
+
+                while (props.MoveNext())
+                {
+                    var prop = props.Current;
+                    Console.WriteLine($"{prop.Name}: {prop.Value}");
+                }
+            }
+        }
+    }
+}
