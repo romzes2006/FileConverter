d41521283ca5877a14f4e89084d0926c6b143992
Starinin Andrey <starininandrey@gmail.com>
2021-12-02 16:09:47 +0300

    Реализация парсинга JSON с одним корневым элементом

--- FileConverter.Lib/JsonConverter.cs
+++ FileConverter.Lib/JsonConverter.cs
@@ -1,30 +1,101 @@
-﻿using System;
+﻿#nullable enable
+
+using System.Collections.Generic;
+using System.Globalization;
 using System.Text.Json;
 
 namespace FileConverter.Lib
 {
     public class JsonConverter
     {
+        private Dictionary<string, string> _dom;
+
+        public JsonConverter()
+        {
+            _dom = new Dictionary<string, string>();
+        }
+        
         public void OnParse(string str)
         {
             using var doc = JsonDocument.Parse(str);
             var root = doc.RootElement;
 
-            var elements = root.EnumerateArray();
+            GetTypeJsonElement(root, null);
+        }
 
-            while (elements.MoveNext())
+        private void GetTypeJsonElement(JsonElement element, string? name)
+        {
+            switch (element.ValueKind)
             {
-                var element = elements.Current;
-                Console.WriteLine(elements);
-
-                var props = element.EnumerateObject();
-
-                while (props.MoveNext())
-                {
-                    var prop = props.Current;
-                    Console.WriteLine($"{prop.Name}: {prop.Value}");
-                }
+                case JsonValueKind.Array:
+                    JsonElementArrayParse(element, name);
+                    break;
+                case JsonValueKind.Object:
+                    JsonElementObjectParse(element, name);
+                    break;
+                case JsonValueKind.Number:
+                    JsonElementNumberParse(element, name);
+                    break;
+                case JsonValueKind.String:
+                    JsonElementStringParse(element, name);
+                    break;
+                case JsonValueKind.False:
+                    break;
+                case JsonValueKind.True:
+                    break;
+                case JsonValueKind.Null:
+                    break;
+                case JsonValueKind.Undefined:
+                    break;
             }
+        }
+
+        private void JsonElementArrayParse(JsonElement element, string? name)
+        {
+            var list = element.EnumerateArray();
+
+            name ??= "element";
+            var index = 1;
+            foreach (var item in list)
+            {
+                GetTypeJsonElement(item, $"{name}_{index.ToString()}");
+                ++index;
+            }
+        }
+
+        private void JsonElementObjectParse(JsonElement element, string? name)
+        {
+            var objects = element.EnumerateObject();
+            foreach (var item in objects)
+            {
+                GetTypeJsonElement(item.Value, item.Name);
+            }
+        }
+
+        private void JsonElementNumberParse(JsonElement element, string? name)
+        {
+            var number = element.GetDecimal();
+            _dom.Add(name, number.ToString(CultureInfo.InvariantCulture));
+        }
+
+        private void JsonElementStringParse(JsonElement element, string? name)
+        {
+            var str = element.GetString();
+            _dom.Add(name, str);
+        }
+
+        public string[] DOMToString()
+        {
+            var result = new string[_dom.Count];
+
+            var index = 0;
+            foreach (var item in _dom)
+            {
+                result[index] = $"{item.Key} -> {item.Value}";
+                ++index;
+            }
+            
+            return result;
         }
     }
 }
