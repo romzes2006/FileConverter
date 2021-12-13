301e4f2992316782fd3659dce61ae68b3370d8e5
Roman Sitnikov <romzes2006@gmail.com>
2021-12-13 20:46:12 +0800

    Пишу парсер файлов.

---
+++ CLI.Lib/PathParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace CLI.Lib
+{
+    public class PathParser
+    {
+        private string _path;
+        PathParser(string path) => _path = path;
+
+        public string GetFileName(string filepath)
+        {
+            return Path.GetFileName(filepath);
+        }
+
+        public string GetFileWitoutEXT(string filename)
+        {
+            var lastposition = filename.LastIndexOf('.');
+            return filename.Substring(0, lastposition - 1);
+        }
+    }
+    
+}
