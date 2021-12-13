1e5389c1c2648a235839959bb3c2db6e76c885da
Starinin Andrey <starininandrey@gmail.com>
2021-12-13 14:35:24 +0300

    Init CLI

---
+++ CLI.Lib/CLI_Attributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CLI.Lib
+{
+    public class CliAttributes
+    {
+        private string[] _attributes;
+
+        public CliAttributes(string[] attributes)
+        {
+            _attributes = attributes;
+        }
+
+        private bool SearchKeyValue(string key, out string value)
+        {
+            for (int i = 0; i < _attributes.Length; i++)
+            {
+                if (_attributes[i] != key) continue;
+                
+                value = _attributes[i + 1];
+                return true;
+            }
+
+            value = null;
+            return false;
+        }
+
+        public bool InputKey(out string path)
+        {
+            return SearchKeyValue("-i", out path);
+        }
+
+        public bool OutputKey(out string path)
+        {
+            return SearchKeyValue("-o", out path);
+        }
+
+        public bool DelimiterKey(out char delimiter)
+        {
+            var res = SearchKeyValue("-s", out var d);
+            delimiter = Convert.ToChar(d);
+            return res;
+        }
+    }
+}
+
